542ad81c6b75bf7d12c4daa24ec74a0d548b504e
hanamilktea <dohan@oregonstate.edu>
2023-02-13 21:08:48 -0800

    added all files - 2/13

---
+++ HelpModel.mjs
@@ -0,0 +1,47 @@
+import mongoose, { isObjectIdOrHexString } from 'mongoose'; 
+import 'dotenv/config'; 
+const Schema = mongoose.Schema; 
+
+mongoose.connect(
+    process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,}
+); 
+
+const db = mongoose.connection; 
+
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose");
+});
+
+// Create a new help message entry
+const createHelpMsg = async (helpMsg) => {
+    const help = new HelpMsg({ helpMsg: helpMsg });
+    return help.save(); 
+};
+
+// Receive/retrieve all help entries in the db
+const findHelpMsgs = async (filter) => {
+    const query = HelpMsg.find(filter);
+    // debug: console.log(query); 
+    return query.exec(); 
+};
+
+// Retrieve a specific help message in the db
+const findHelpMsg = async(_id) => {
+    const query = HelpMsg.findOne( {_id: _id } );
+    return query.exec(); 
+};
+
+const HelpMsgSchema = new Schema(
+    {
+        helpMsg: {
+            type: String,
+            required: true,
+        }
+    }
+);
+
+const HelpMsg = mongoose.model("helpMsgs", HelpMsgSchema); 
+
+export { createHelpMsg, findHelpMsg, findHelpMsgs }; 
---
+++ index.mjs
@@ -0,0 +1,73 @@
+import express from 'express'; 
+import bodyParser from 'body-parser'; 
+import 'dotenv/config';  
+
+import * as HelpMsgs from './HelpModel.mjs'; 
+
+const app = express(); 
+app.set('view engine', 'pug');
+const PORT = process.env.PORT || 3001; 
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: true})); 
+
+app.listen(PORT, () => {
+    console.log(`Help Microservice listening on port ${PORT}...`); 
+});
+
+// log errors just in case
+app.use((err, req, res, next) => {
+    console.error(err.status); 
+    res.status(500).send({ error: 'Something went wrong!' }); 
+}); 
+
+// Add help messages to the db
+app.post('/add-help', (req, res) => {
+    HelpMsgs.createHelpMsg(req.body.helpMsg)
+    // if successful
+    .then (helpMsg => {
+        res.status(201).json(helpMsg); 
+    })
+    // else catch the error
+    .catch(error => {
+        console.error(error); 
+        res.status(400).json({ Error: 'Invalid request' }); 
+    }); 
+}); 
+
+// Request/get all help messages in the db
+app.get("/get-all", (req, res) => {
+    let filter = {}; 
+    HelpMsgs.findHelpMsgs(filter)
+        .then(helpMsg => {
+            if (helpMsg !== null) {
+                // debug: console.log(helpMsg);
+                res.status(200).json(helpMsg); 
+            } else {
+                res.status(404).json({ Error: "No Help Messages available." }); 
+            }
+        })
+        .catch(error => {
+            console.error(error); 
+            res.status(400).json({ Error: "Request failed." }); 
+        }); 
+}); 
+
+// Finds one help message and return its contents
+app.get('/:id', (req, res) => {
+    const helpMsgId = req.params.id; 
+    HelpMsgs.findHelpMsg(helpMsgId)
+    .then(helpMsg => {
+        if (helpMsg !== null) {  // if the help message exists
+            // res.status(200).json(helpMsg); 
+            res.render('index', { title: 'Matching Help Message', message: helpMsg.helpMsg }); 
+        } else {
+            res.status(404).json({ Error: "Help message not found." }); 
+        }
+    })
+    .catch(error => {
+        console.error(error);
+        res.status(404).json({ Error: "Invalid request" }); 
+    }); 
+}); 
+
